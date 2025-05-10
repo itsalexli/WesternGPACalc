dd5f5600f74c3cb066a2aac96c8fde60a658c592
itsalexli <alex.liyou@gmail.com>
2025-05-10 19:29:20 -0400

    Initial Commit

---
+++ WesternGPACalc/WesternGPACalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesternGPACalcApp.swift
+//  WesternGPACalc
+//
+//  Created by Alexander Li on 2025-05-10.
+//
+
+import SwiftUI
+
+@main
+struct WesternGPACalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WesternGPACalcTests/WesternGPACalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  WesternGPACalcTests.swift
+//  WesternGPACalcTests
+//
+//  Created by Alexander Li on 2025-05-10.
+//
+
+import Testing
+@testable import WesternGPACalc
+
+struct WesternGPACalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
